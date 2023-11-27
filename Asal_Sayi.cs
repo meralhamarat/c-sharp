c22055edf51ba0751025a8b62f1133f2fdedaac5
Meral Hamarat <114569285+meralhamarat@users.noreply.github.com>
2023-11-28 00:12:27 +0300

    Create Asal_Sayi.cs

---
+++ Asal_Sayi.cs
@@ -0,0 +1,47 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir sayı giriniz: ");
+        string userInput = Console.ReadLine();
+
+        if (int.TryParse(userInput, out int sayi))
+        {
+            bool asalMi = IsAsal(sayi);
+
+            if (asalMi)
+            {
+                Console.WriteLine($"{sayi} bir asal sayıdır.");
+            }
+            else
+            {
+                Console.WriteLine($"{sayi} bir asal sayı değildir.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Geçerli bir sayı girmelisiniz.");
+        }
+    }
+
+    // Bir sayının asal olup olmadığını kontrol eden fonksiyon
+    static bool IsAsal(int sayi)
+    {
+        if (sayi <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(sayi); i++)
+        {
+            if (sayi % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
