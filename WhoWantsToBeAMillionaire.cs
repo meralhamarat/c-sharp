ed224e305f9bd38832c92a435ac4625d5915f806
Meral Hamarat <114569285+meralhamarat@users.noreply.github.com>
2023-12-01 00:31:46 +0300

    Update WhoWantsToBeAMillionaire.cs

--- WhoWantsToBeAMillionaire.cs
+++ WhoWantsToBeAMillionaire.cs
@@ -15,8 +15,8 @@ class MilyonerUygulamasi
     {
         Console.ForegroundColor = ConsoleColor.Magenta;
         Console.WriteLine("╔══════════════════════════════════════════════════════╗");
-        Console.WriteLine("║                 M İ L Y O N E R                      ║");
-        Console.WriteLine("║                  Y A R I Ş M A S I                   ║");
+        Console.WriteLine("║                 M I L Y O N E R                      ║");
+        Console.WriteLine("║                  Y A R I S M A S I                   ║");
         Console.WriteLine("╚══════════════════════════════════════════════════════╝");
         Console.ResetColor();
         Console.WriteLine("Milyoner Yarismasina hos geldiniz!");
@@ -25,30 +25,31 @@ class MilyonerUygulamasi
         Console.WriteLine("yanlis bir cevap verdiginizde elenebilir ve kazandiginiz miktarı kaybedebilirsiniz.");
         Console.WriteLine("Basarilar dileriz!\n");
 
+
         string[] sorular = {
-            "1. Türkiye'nin başkenti nedir?",
-            "2. Hangi gezegen Güneş Sistemi'nde üçüncü sıradadır?",
-            "3. Hangi renk gökkuşağının ilk rengidir?",
-            "4. Hangi yıl Türkiye'de Cumhuriyet ilan edilmiştir?",
-            "5. İnsan vücudunda kaç adet kalp bulunur?",
-            "6. Hangi elementin simgesi 'O' harfi ile gösterilir?",
-            "7. Hangi yıl Leonardo da Vinci doğmuştur?",
-            "8. Türkiye'nin en yüksek dağı hangisidir?",
-            "9. Hangi hayvan memeli değildir?",
-            "10. Hangi gezegen 'Akrep Kuyruğu'na sahiptir?"
+            "1. Turkiye'nin baskenti nedir?",
+            "2. Hangi gezegen Gunes Sistemi'nde ucuncu siradadir?",
+            "3. Hangi renk gokkusaginin ilk rengidir?",
+            "4. Hangi yil Turkiye'de Cumhuriyet ilan edilmistir?",
+            "5. Insan vucudunda kac adet kalp bulunur?",
+            "6. Hangi elementin simgesi 'O' harfi ile gosterilir?",
+            "7. Hangi yil Leonardo da Vinci dogmustur?",
+            "8. Turkiye'nin en yuksek dagi hangisidir?",
+            "9. Hangi hayvan memeli degildir?",
+            "10. Hangi gezegen 'Akrep Kuyrugu'na sahiptir?"
         };
 
         string[][] cevaplar = {
-            new string[] {"A) İstanbul", "B) Ankara", "C) İzmir", "D) Bursa"},
-            new string[] {"A) Mars", "B) Venüs", "C) Dünya", "D) Jüpiter"},
-            new string[] {"A) Kırmızı", "B) Mavi", "C) Sarı", "D) Yeşil"},
+            new string[] {"A) Istanbul", "B) Ankara", "C) Izmir", "D) Bursa"},
+            new string[] {"A) Mars", "B) Venus", "C) Dunya", "D) Jupiter"},
+            new string[] {"A) Kirmizi", "B) Mavi", "C) Sari", "D) Yesil"},
             new string[] {"A) 1920", "B) 1921", "C) 1922", "D) 1923"},
             new string[] {"A) 1", "B) 2", "C) 4", "D) 6"},
             new string[] {"A) Hidrojen", "B) Oksijen", "C) Karbon", "D) Azot"},
             new string[] {"A) 1400", "B) 1452", "C) 1500", "D) 1550"},
-            new string[] {"A) Ağrı Dağı", "B) Erciyes", "C) Cilo Dağı", "D) Süphan Dağı"},
-            new string[] {"A) Fil", "B) Kartal", "C) Yılan", "D) Köpek"},
-            new string[] {"A) Jüpiter", "B) Mars", "C) Neptün", "D) Uranüs"}
+            new string[] {"A) Agri Dagi", "B) Erciyes", "C) Cilo Dagi", "D) Suphan Dagi"},
+            new string[] {"A) Fil", "B) Kartal", "C) Yilan", "D) Kopek"},
+            new string[] {"A) Jupiter", "B) Mars", "C) Neptun", "D) Uranus"}
         };
 
         string[] dogruCevaplar = {"B", "C", "A", "D", "A", "B", "B", "A", "C", "C"};
@@ -64,58 +65,58 @@ class MilyonerUygulamasi
         {
             Console.WriteLine(sorular[i]);
 
-            // Seçenekleri yazdır
+            // Secenekleri yazdir
             for (int j = 0; j < cevaplar[i].Length; j++)
             {
                 Console.WriteLine(cevaplar[i][j]);
             }
 
-            Console.Write("Cevabınızı girin (A, B, C veya D): ");
-            string kullaniciCevabi = Console.ReadLine().ToUpper(); // Küçük veya büyük harf girişine karşı duyarlı değil
+            Console.Write("Cevabinizi girin (A, B, C veya D): ");
+            string kullaniciCevabi = Console.ReadLine().ToUpper(); // Kucuk veya buyuk harf girisine karsi duyarli degil
 
             if (kullaniciCevabi == dogruCevaplar[i])
             {
-                // Soruyu doğru cevaplayınca puanı güncelle
+                // Soruyu dogru cevaplayinca puani guncelle
                 puan += 10000;
-                Console.WriteLine($"Doğru! Kazandığınız tutar: {puan} TL\n");
+                Console.WriteLine($"Dogru! Kazandiniz tutar: {puan} TL\n");
 
-                // Baraj sorularını kontrol et ve devam et
+                // Baraj sorularini kontrol et ve devam et
                 if ((i + 1 == barajSoru1 || i + 1 == barajSoru2) && puan >= barajPuan1)
                 {
                     if (i + 1 == barajSoru1)
                     {
-                        barajPuan1 = puan; // Baraj sorusunu doğru bildiyse kazandığı parayı baraj puanına eşitle
-                        Console.WriteLine($"Tebrikler! {barajSoru1}. soruyu doğru bildiniz ve {barajPuan1} TL kazandınız.");
+                        barajPuan1 = puan; // Baraj sorusunu dogru bildiyse kazandigi parayi baraj puanina esitle
+                        Console.WriteLine($"Tebrikler! {barajSoru1}. soruyu dogru bildiniz ve {barajPuan1} TL kazandiniz.");
                     }
 
                     if (i + 1 == barajSoru2)
                     {
-                        barajPuan2 = puan; // Baraj sorusunu doğru bildiyse kazandığı parayı baraj puanına eşitle
-                        Console.WriteLine($"Tebrikler! {barajSoru2}. soruyu doğru bildiniz ve {barajPuan2} TL kazandınız.");
+                        barajPuan2 = puan; // Baraj sorusunu dogru bildiyse kazandigi parayi baraj puanina esitle
+                        Console.WriteLine($"Tebrikler! {barajSoru2}. soruyu dogru bildiniz ve {barajPuan2} TL kazandiniz.");
                     }
                 }
             }
             else
             {
-                // Yanlış cevap durumunda oyunu bitir ve baraj paralarını kontrol et
-                Console.WriteLine($"Yanlış. Doğru cevap: {dogruCevaplar[i]}.\n");
+                // Yanlis cevap durumunda oyunu bitir ve baraj paralarini kontrol et
+                Console.WriteLine($"Yanlis. Dogru cevap: {dogruCevaplar[i]}.\n");
 
                 if (i + 1 == barajSoru1 || i + 1 == barajSoru2)
                 {
-                    Console.WriteLine($"Yarışma sona erdi. Kazandığınız tutar: {puan} TL");
+                    Console.WriteLine($"Yarisma sona erdi. Kazandiniz tutar: {puan} TL");
                     break;
                 }
             }
 
-            // Yanlış cevap durumunda oyunu bitir
+            // Yanlis cevap durumunda oyunu bitir
             if (kullaniciCevabi != dogruCevaplar[i])
             {
-                Console.WriteLine($"Yarışma sona erdi. Kazandığınız tutar: {puan} TL");
+                Console.WriteLine($"Yarisma sona erdi. Kazandiniz tutar: {puan} TL");
                 break;
             }
         }
 
-        // Konsol penceresini kapatmadan önce kullanıcı girişini beklet
+        // Konsol penceresini kapatmadan once kullanicinin girisini beklet
         Console.ReadLine();
     }
 }
