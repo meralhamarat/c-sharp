6e1ee1ea1ff13c1f8ac744f99d6a2fb436be39f1
Meral Hamarat <114569285+meralhamarat@users.noreply.github.com>
2023-12-08 23:51:58 +0300

    Create Cember.cs

---
+++ Cember.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double radius;
+
+        do
+        {
+            Console.Write("Dairenin yarıçapını girin: ");
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out radius) && radius > 0)
+            {
+                // Geçerli bir yarıçap değeri alındı, işlemlere devam et
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz giriş. Pozitif bir sayı girin.");
+            }
+
+        } while (true);
+
+        // Alan ve çevre hesaplamaları
+        double area = Math.PI * Math.Pow(radius, 2);
+        double circumference = 2 * Math.PI * radius;
+
+        // Sonuçları ekrana yazdır
+        Console.WriteLine($"Dairenin Alanı: {area:F2}");
+        Console.WriteLine($"Dairenin Çevresi: {circumference:F2}");
+
+        Console.ReadLine();
+    }
+}
