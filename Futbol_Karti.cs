276b3142aa2440e99084d83655bac53441b787ea
Meral Hamarat <114569285+meralhamarat@users.noreply.github.com>
2023-12-10 02:29:00 +0300

    Update Futbol_Karti.cs

--- Futbol_Karti.cs
+++ Futbol_Karti.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 class FutbolOyuncusu
 {
@@ -32,13 +33,26 @@ class Program
 {
     static void Main()
     {
-        FutbolOyuncusu oyuncu1 = new FutbolOyuncusu("Lionel Messi", "Paris Saint-Germain", "Forvet", 90, 80);
-        FutbolOyuncusu oyuncu2 = new FutbolOyuncusu("Cristiano Ronaldo", "Manchester United", "Forvet", 88, 85);
-        FutbolOyuncusu oyuncu3 = new FutbolOyuncusu("Arda Turan", "Galatasaray", "Orta Saha", 85, 75);
+        List<FutbolOyuncusu> futbolcular = new List<FutbolOyuncusu>
+        {
+            new FutbolOyuncusu("Lionel Messi", "Paris Saint-Germain", "Forvet", 90, 80),
+            new FutbolOyuncusu("Cristiano Ronaldo", "Manchester United", "Forvet", 88, 85),
+            new FutbolOyuncusu("Neymar", "Paris Saint-Germain", "Forvet", 92, 75),
+            new FutbolOyuncusu("Sergio Ramos", "Paris Saint-Germain", "Defans", 78, 88),
+            new FutbolOyuncusu("Kevin De Bruyne", "Manchester City", "Orta Saha", 86, 82),
+            new FutbolOyuncusu("Mohamed Salah", "Liverpool", "Forvet", 87, 79),
+            new FutbolOyuncusu("Arda Turan", "Galatasaray", "Orta Saha", 85, 75),
+            new FutbolOyuncusu("Burak Yılmaz", "Beşiktaş", "Forvet", 82, 77),
+            new FutbolOyuncusu("Hakan Çalhanoğlu", "AC Milan", "Orta Saha", 84, 78),
+            new FutbolOyuncusu("Merih Demiral", "Atalanta", "Defans", 80, 85),
+            new FutbolOyuncusu("Ozan Tufan", "Fenerbahçe", "Orta Saha", 79, 81),
+            new FutbolOyuncusu("Çağlar Söyüncü", "Leicester City", "Defans", 81, 83)
+        };
 
-        oyuncu1.BilgileriYazdir();
-        oyuncu2.BilgileriYazdir();
-        oyuncu3.BilgileriYazdir();
+        foreach (var oyuncu in futbolcular)
+        {
+            oyuncu.BilgileriYazdir();
+        }
 
         Console.ReadLine();
     }
