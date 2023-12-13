3bbb1fc8747210f7856c6bbab952bd97981657e4
Meral Hamarat <114569285+meralhamarat@users.noreply.github.com>
2023-12-14 02:15:48 +0300

    Update Tarih_Hesaplama.cs
    in the second version, the code explicitly handles both adding and subtracting days based on the user's choice.

--- Tarih_Hesaplama.cs
+++ Tarih_Hesaplama.cs
@@ -1,4 +1,4 @@
-// Aşağıdaki C# kodu, kullanıcıdan başlangıç tarihini ve gün sayısını alır, ardından belirtilen gün sayısı kadar gün ekleyerek yeni tarihi hesaplar ve ekrana yazdırır.
+// Aşağıdaki C# kodu, kullanıcıdan başlangıç tarihini ve gün sayısını alır, ardından belirtilen gün sayısı kadar gün ekleyerek veya çıkararak yeni tarihi hesaplar ve ekrana yazdırır.
 
 /* ************************************************************************** */
 /*                                                                            */
@@ -19,11 +19,29 @@ class Program
         Console.Write("Başlangıç tarihini girin (GG.AA.YYYY): ");
         string baslangicTarihiStr = Console.ReadLine();
 
-        Console.Write("Kaç gün eklemek istiyorsunuz: ");
+        Console.Write("Gün eklemek için (E) veya gün çıkarmak için (C) seçin: ");
+        char ekleCikarSecimi = char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine(); // Yeni satıra geç
+
+        if (ekleCikarSecimi == 'E')
+        {
+            Console.Write("Kaç gün eklemek istiyorsunuz: ");
+        }
+        else if (ekleCikarSecimi == 'C')
+        {
+            Console.Write("Kaç gün çıkarmak istiyorsunuz: ");
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz seçim. Program sonlandırılıyor.");
+            Console.ReadLine();
+            return;
+        }
+
         int gunSayisi = Convert.ToInt32(Console.ReadLine());
 
         // Tarih hesaplama fonksiyonunu çağır
-        string yeniTarih = TarihHesapla(baslangicTarihiStr, gunSayisi);
+        string yeniTarih = TarihHesapla(baslangicTarihiStr, ekleCikarSecimi, gunSayisi);
 
         // Sonucu ekrana yazdır
         Console.WriteLine($"Yeni tarih: {yeniTarih}");
@@ -32,13 +50,13 @@ class Program
         Console.ReadLine();
     }
 
-    static string TarihHesapla(string baslangicTarihi, int gunSayisi)
+    static string TarihHesapla(string baslangicTarihi, char ekleCikarSecimi, int gunSayisi)
     {
         // Başlangıç tarihini DateTime nesnesine çevir
         DateTime baslangicTarihiObj = DateTime.ParseExact(baslangicTarihi, "dd.MM.yyyy", null);
 
-        // Belirtilen gün sayısı kadar gün ekleyerek yeni tarihi bul
-        DateTime yeniTarihObj = baslangicTarihiObj.AddDays(gunSayisi);
+        // Belirtilen gün sayısı kadar gün ekleyerek veya çıkararak yeni tarihi bul
+        DateTime yeniTarihObj = ekleCikarSecimi == 'E' ? baslangicTarihiObj.AddDays(gunSayisi) : baslangicTarihiObj.AddDays(-gunSayisi);
 
         // Yeni tarihi formatlı bir şekilde döndür
         return yeniTarihObj.ToString("dd.MM.yyyy");
