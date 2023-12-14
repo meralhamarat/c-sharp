3316c5564eba3e65909481caab605706f84ddf68
Meral Hamarat <114569285+meralhamarat@users.noreply.github.com>
2023-12-14 03:03:03 +0300

    Add files via upload

---
+++ ex02/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Adam Asmaca Oyunu");
+        
+        string[] kelimeler = { "bilgisayar", "programlama", "csharp", "geliştirme", "oyun" };
+        Random random = new Random();
+        string secilenKelime = kelimeler[random.Next(0, kelimeler.Length)];
+        
+        char[] dogruTahminler = new char[secilenKelime.Length];
+        for (int i = 0; i < dogruTahminler.Length; i++)
+        {
+            dogruTahminler[i] = '_';
+        }
+        
+        int kalanCan = 6;
+        
+        while (kalanCan > 0)
+        {
+            Console.WriteLine($"Kelime: {string.Join(" ", dogruTahminler)}");
+            Console.WriteLine($"Kalan Can: {kalanCan}");
+            
+            Console.Write("Bir harf girin: ");
+            char tahmin = Console.ReadLine()[0];
+            
+            if (secilenKelime.Contains(tahmin))
+            {
+                for (int i = 0; i < secilenKelime.Length; i++)
+                {
+                    if (secilenKelime[i] == tahmin)
+                    {
+                        dogruTahminler[i] = tahmin;
+                    }
+                }
+                
+                if (!dogruTahminler.Contains('_'))
+                {
+                    Console.WriteLine("Tebrikler, kelimeyi doğru tahmin ettiniz!");
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hatalı tahmin, bir can kaybettiniz.");
+                kalanCan--;
+            }
+        }
+        
+        if (kalanCan == 0)
+        {
+            Console.WriteLine($"Üzgünüm, canlarınız tükendi. Doğru kelime: {secilenKelime}");
+        }
+        
+        Console.ReadLine();
+    }
+}
