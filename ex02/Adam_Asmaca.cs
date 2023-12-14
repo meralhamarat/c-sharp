51c424d2de04f5a5215ed1135453ba31988b5c08
Meral Hamarat <114569285+meralhamarat@users.noreply.github.com>
2023-12-14 03:08:07 +0300

    Update Adam_Asmaca.cs
    I changed the words and removed Turkish characters

--- ex02/Adam_Asmaca.cs
+++ ex02/Adam_Asmaca.cs
@@ -1,31 +1,31 @@
-﻿using System;
+using System;
 
 class Program
 {
     static void Main()
     {
         Console.WriteLine("Adam Asmaca Oyunu");
-        
-        string[] kelimeler = { "bilgisayar", "programlama", "csharp", "geliştirme", "oyun" };
+
+        string[] kelimeler = { "elma", "portakal", "kalem", "kitap", "masa" };
         Random random = new Random();
         string secilenKelime = kelimeler[random.Next(0, kelimeler.Length)];
-        
+
         char[] dogruTahminler = new char[secilenKelime.Length];
         for (int i = 0; i < dogruTahminler.Length; i++)
         {
             dogruTahminler[i] = '_';
         }
-        
+
         int kalanCan = 6;
-        
+
         while (kalanCan > 0)
         {
             Console.WriteLine($"Kelime: {string.Join(" ", dogruTahminler)}");
             Console.WriteLine($"Kalan Can: {kalanCan}");
-            
+
             Console.Write("Bir harf girin: ");
-            char tahmin = Console.ReadLine()[0];
-            
+            char tahmin = Char.ToLower(Console.ReadLine()[0]); // Küçük harfe çevirme
+
             if (secilenKelime.Contains(tahmin))
             {
                 for (int i = 0; i < secilenKelime.Length; i++)
@@ -35,7 +35,7 @@ class Program
                         dogruTahminler[i] = tahmin;
                     }
                 }
-                
+
                 if (!dogruTahminler.Contains('_'))
                 {
                     Console.WriteLine("Tebrikler, kelimeyi doğru tahmin ettiniz!");
@@ -48,12 +48,12 @@ class Program
                 kalanCan--;
             }
         }
-        
+
         if (kalanCan == 0)
         {
             Console.WriteLine($"Üzgünüm, canlarınız tükendi. Doğru kelime: {secilenKelime}");
         }
-        
+
         Console.ReadLine();
     }
 }
