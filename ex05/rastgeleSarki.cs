92e454b98a01670f088075f5d314ebae7c6e0b79
Meral Hamarat <114569285+meralhamarat@users.noreply.github.com>
2023-12-22 05:16:14 +0300

    Add files via upload

---
+++ ex05/rastgeleSarki.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> sarkiListesi = new List<string>
+        {
+            "Musa Gülsah - Beni Kandırdın",
+            "Robert Hatemo - Senden Çok Var",
+            "İsmail YK - Git Hadi Git",
+            "Ceza - Holocoust",
+            "Musa Gülsah - Göçtü Gitti",
+            "Robert Hatemo - Aşk Çiçeğim",
+            "İsmail YK - Bas Gaza",
+            "Ceza - Araturka Faslı",
+            // ... diğer şarkılar
+        };
+
+        Random random = new Random();
+        int randomIndex = random.Next(sarkiListesi.Count);
+        string randomSarki = sarkiListesi[randomIndex];
+
+        Console.WriteLine($"İşte sana bir şarkı önerisi: {randomSarki}");
+
+        Console.ReadLine(); // Konsolun kapatılmaması için bekleme
+    }
+}
