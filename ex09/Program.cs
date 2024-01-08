a230b979dc463813844fb94d4c97c574e1dc8a44
Meral Hamarat <meralhamarat24@gmail.com>
2024-01-08 17:56:35 +0300

    Add files via upload

---
+++ ex09/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    // Delege tanımı
+    delegate void MyDelegate(int x, int y);
+
+    // Delege ile uyumlu olan metotlar
+    static void Toplama(int x, int y)
+    {
+        Console.WriteLine($"{x} + {y} = {x + y}");
+    }
+
+    static void Carpma(int x, int y)
+    {
+        Console.WriteLine($"{x} * {y} = {x * y}");
+    }
+
+    static void Bolme(int x, int y)
+    {
+        if (y != 0)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+        else
+        {
+            Console.WriteLine("Bölme işlemi tanımsız (y = 0).");
+        }
+    }
+
+    public Program()
+    {
+        // Delege tanımlama ve metotları ekleme
+        MyDelegate delege = null;
+        delege += new MyDelegate(Toplama);
+        delege += new MyDelegate(Carpma);
+        delege += new MyDelegate(Bolme);
+
+        int X, Y;
+        X = 18;
+        Y = 3;
+
+        // Delegeyi çağırma
+        delege(X, Y);
+    }
+
+    static void Main()
+    {
+        new Program();
+        Console.ReadLine();
+    }
+}
