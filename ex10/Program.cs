c3ef797df089b88ee94ca8db1fcbaec20e5612ea
Meral Hamarat <meralhamarat24@gmail.com>
2024-01-10 01:37:11 +0300

    Update Program.cs

--- ex10/Program.cs
+++ ex10/Program.cs
@@ -11,14 +11,14 @@ namespace ReferanceTypes
         
             x = y;
             y = 30;
-            Console.WriteLine(x);
+            Console.WriteLine("x : " +x);
             
             // array, class, interface, delegate, string, object, dynamic reference types backed by heap
             int [] numbers1 = new int [] {1, 2, 3};
             int [] numbers2 = new int [] {4, 5, 6};
             numbers1 = numbers2;
             numbers2[0] = 400;
-            Console.WriteLine(numbers1[0]);
+            Console.WriteLine("numbers1 : " +numbers1[0]);
         }
     }
 }
