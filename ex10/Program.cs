4a1a299f9fff988db7a37f80bfccfe8d1523307d
Meral Hamarat <meralhamarat24@gmail.com>
2024-01-09 23:52:03 +0300

    Referance types exercise

---
+++ ex10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ReferanceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+        
+            x = y;
+            y = 30;
+            Console.WriteLine(x);
+        }
+       
+    }
+}
